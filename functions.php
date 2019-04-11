4b329181bda98296a0a957f34f5f29f457f49019
Ben Parry <benparrywork@gmail.com>
2019-04-12 00:08:11 +1000

    Adding basic placeholder website with particles.js

--- functions.php
+++ functions.php
@@ -3,11 +3,21 @@
 class UIUX_Wordpress_theme {
 
 	function __construct() {
+		add_action( 'wp_enqueue_scripts', array($this, 'load_js') );
+		add_action( 'wp_enqueue_scripts', array($this, 'load_css') );
+	}
 
+	function load_js() {
+		wp_enqueue_script( 'particles-js', get_template_directory_uri().'/static/js/particles.min.js', false );
+		wp_enqueue_script( 'theme-js', get_template_directory_uri().'/static/js/things.js', false, false, true );
+	}
+
+	function load_css() {
+		wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), false );
 	}
 
 }
 
-
+$uiux_theme = new UIUX_Wordpress_theme();
 
 ?>
--- index.php
+++ index.php
@@ -2,9 +2,28 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>UIUX.me</title>
+	<title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+
+	<!--
+	
+	 __  __     __     __  __     __  __     __    __     ______    
+	/\ \/\ \   /\ \   /\ \/\ \   /\_\_\_\   /\ "-./  \   /\  ___\   
+	\ \ \_\ \  \ \ \  \ \ \_\ \  \/_/\_\/_  \ \ \-./\ \  \ \  __\   
+	 \ \_____\  \ \_\  \ \_____\   /\_\/\_\  \ \_\ \ \_\  \ \_____\ 
+	  \/_____/   \/_/   \/_____/   \/_/\/_/   \/_/  \/_/   \/_____/ 
+	                                                                
+	-->
+
+	<?php wp_head(); ?>
+
 </head>
 <body>
 	
+	<h1>UIUX</h1>
+
+	<div id="particles"></div>
+
+	<?php wp_footer(); ?>
+		
 </body>
 </html>
